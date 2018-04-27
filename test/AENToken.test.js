bfeb8ed733278a860b7415bf87366e0dbdc5752b
Geoffrey Tipton <tipton@mysecretroom.com>
2018-04-27 19:47:35 +0800

    Full tested with tes results and set to full ERC20 upgraded

---
+++ test/AENToken.test.js
@@ -0,0 +1,41 @@
+const Token = artifacts.require('./AENToken.sol'); //must be the name of the contract file
+const suite = require('../node_modules/token-test-suite/lib/suite');
+
+contract('Total Supply', function(accounts) {
+	// it("Get Values", async function() {
+	//   supply = await Token.totalSupply();
+	//   assert.equal(supply, 10000000, "New crowdsale was not created");
+	// });
+
+
+	let options = {
+		// accounts to test with, accounts[0] being the contract owner
+		accounts: accounts,
+
+		// factory method to create new token contract
+		create: async function () {
+			return await Token.new();
+		},
+
+		transfer: async function (token, to, amount) {
+			return await token.transfer(to, amount, { from: accounts[0] });
+		},
+
+		// token info to test
+		name: 'AEN.',
+		symbol: 'AEN',
+		decimals: 8,
+
+		// initial state to test
+		initialSupply: 400000000000000000,
+		initialBalances: [
+			[accounts[0], 400000000000000000]
+		],
+		initialAllowances: [
+			[accounts[0], accounts[1], 0]
+		]
+	};
+ 	suite(options);
+
+});
+
